7f55ec72ecf88fc1c29ea0e2791ed09b4d1b6bdf
MrSpaceMan1 <bartek99q@gmail.com>
2023-08-21 17:33:19 +0200

    Actual logic, readme

--- src/main/java/com/github/mrspaceman1/colorcodedactivatorrails/ColorcodedDetectorRail.java
+++ src/main/java/com/github/mrspaceman1/colorcodedactivatorrails/ColorcodedDetectorRail.java
@@ -1,13 +1,28 @@
 package com.github.mrspaceman1.colorcodedactivatorrails;
 
+import net.minecraft.block.BarrelBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.DetectorRailBlock;
+import net.minecraft.block.entity.BarrelBlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import net.minecraft.entity.vehicle.MinecartEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.TypeFilter;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+
+import static net.minecraft.block.DetectorRailBlock.POWERED;
 
 public class ColorcodedDetectorRail {
 
@@ -38,5 +53,41 @@ public class ColorcodedDetectorRail {
             }
         }
         return false;
+    }
+
+    public static void colorcodedActivation(World world, BlockPos pos, BlockState state, int flags, CallbackInfoReturnable<Boolean> cir){
+        if(!(state.getBlock() instanceof DetectorRailBlock detectorRailBlock)) return;
+        if(!state.get(POWERED)) return;
+
+        BlockPos configBarrelPos = pos.offset(Direction.DOWN, 2);
+        if(!(world.getBlockState(configBarrelPos).getBlock() instanceof BarrelBlock configBarrelBlock)) return;
+        BarrelBlockEntity barrelEntity = (BarrelBlockEntity) world.getBlockEntity(configBarrelPos);
+
+        Set<ItemStack> barrelItemStackSet = new HashSet<>();
+        for (int i=0; i<barrelEntity.size(); i++){
+            ItemStack itemStack = barrelEntity.getStack(i);
+            if(!itemStack.isEmpty())
+                barrelItemStackSet.add(itemStack);
+        }
+
+        List<AbstractMinecartEntity> minecarts = getCartsAtPos((World) (Object) world, pos);
+        for(AbstractMinecartEntity minecart : minecarts){
+            if(!(minecart instanceof MinecartEntity)) continue;
+            Optional<Entity> optionalPassenger = Optional.ofNullable(minecart.getFirstPassenger());
+
+            if(optionalPassenger.isPresent() && optionalPassenger.get() instanceof PlayerEntity player){
+                Set<ItemStack> playerItemStackSet = new HashSet<>();
+                PlayerInventory inventory = player.getInventory();
+
+                for(int i=0; i<inventory.size(); i++){
+                    ItemStack itemStack = inventory.getStack(i);
+                    if(!itemStack.isEmpty())
+                        playerItemStackSet.add(itemStack);
+                }
+
+                if(checkInventoryContainsItemStacks(barrelItemStackSet, playerItemStackSet)) return;
+            }
+        }
+        cir.setReturnValue(world.setBlockState(pos, state.with(POWERED, false), flags));
     }
 }
--- src/main/java/com/github/mrspaceman1/colorcodedactivatorrails/mixin/WorldMixin.java
+++ src/main/java/com/github/mrspaceman1/colorcodedactivatorrails/mixin/WorldMixin.java
@@ -1,18 +1,8 @@
 package com.github.mrspaceman1.colorcodedactivatorrails.mixin;
 
-import net.minecraft.block.BarrelBlock;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.DetectorRailBlock;
-import net.minecraft.block.entity.BarrelBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.entity.vehicle.AbstractMinecartEntity;
-import net.minecraft.entity.vehicle.MinecartEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,55 +10,14 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import java.util.*;
 
-import static com.github.mrspaceman1.colorcodedactivatorrails.ColorcodedDetectorRail.checkInventoryContainsItemStacks;
-import static com.github.mrspaceman1.colorcodedactivatorrails.ColorcodedDetectorRail.getCartsAtPos;
-import static net.minecraft.block.DetectorRailBlock.POWERED;
+import static com.github.mrspaceman1.colorcodedactivatorrails.ColorcodedDetectorRail.*;
 
 @Mixin(World.class)
 public abstract class WorldMixin {
 
-    @Shadow public abstract boolean setBlockState(BlockPos pos, BlockState state, int flags);
-
-    @Shadow public abstract BlockState getBlockState(BlockPos pos);
-
-    @Shadow @Nullable public abstract BlockEntity getBlockEntity(BlockPos pos);
-
     @Inject(at=@At("HEAD"), method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z", cancellable = true)
     public void onSetBlockState(BlockPos pos, BlockState state, int flags, CallbackInfoReturnable<Boolean> cir){
-        if(!(state.getBlock() instanceof DetectorRailBlock detectorRailBlock)) return;
-        if(!state.get(POWERED)) return;
-
-        BlockPos configBarrelPos = pos.offset(Direction.DOWN, 2);
-        if(!(this.getBlockState(configBarrelPos).getBlock() instanceof BarrelBlock configBarrelBlock)) return;
-        BarrelBlockEntity barrelEntity = (BarrelBlockEntity) this.getBlockEntity(configBarrelPos);
-
-        Set<ItemStack> barrelItemStackSet = new HashSet<>();
-        for (int i=0; i<barrelEntity.size(); i++){
-            ItemStack itemStack = barrelEntity.getStack(i);
-            if(!itemStack.isEmpty())
-                barrelItemStackSet.add(itemStack);
-        }
-
-        List<AbstractMinecartEntity> minecarts = getCartsAtPos((World) (Object) this, pos);
-        for(AbstractMinecartEntity minecart : minecarts){
-            if(!(minecart instanceof MinecartEntity)) continue;
-            Optional<Entity> optionalPassenger = Optional.ofNullable(minecart.getFirstPassenger());
-
-            if(optionalPassenger.isPresent() && optionalPassenger.get() instanceof PlayerEntity player){
-                Set<ItemStack> playerItemStackSet = new HashSet<>();
-                PlayerInventory inventory = player.getInventory();
-
-                for(int i=0; i<inventory.size(); i++){
-                    ItemStack itemStack = inventory.getStack(i);
-                    if(!itemStack.isEmpty())
-                        playerItemStackSet.add(itemStack);
-                }
-
-                if(checkInventoryContainsItemStacks(barrelItemStackSet, playerItemStackSet)) return;
-            }
-        }
-        cir.setReturnValue(this.setBlockState(pos, state.with(POWERED, false), flags));
+        colorcodedActivation((World) (Object) this, pos, state, flags, cir);
     }
 }
